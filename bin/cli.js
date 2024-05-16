aae7248276858688cde603867d1410a6268fe4f4
Henock MASS <henockmassamba@gmail.com>
2024-05-16 17:28:35 +0100

    bin

---
+++ bin/cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const { execSync } = require("child_process");
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, {
+      stdio: "inherit",
+    });
+  } catch (error) {
+    console.log(`Failed to execute ${command}`, error);
+    return false;
+  }
+  return true;
+};
+const repoName = process.argv[2] ?? null;
+const gitChekoutCommand = `git clone --depth 1 https://github.com/Masshen/react-native-dill.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Clonning the repository with name ${repoName} `);
+
+const checkout = runCommand(gitChekoutCommand);
+if (!checkout) process.exit(-1);
+console.log(`Installing dependencies for ${repoName}`);
+const installDeps = runCommand(installDepsCommand);
+if (!installDeps) process.exit(-1);
+console.log(`cd ${repoName} && npx react-native start`);
